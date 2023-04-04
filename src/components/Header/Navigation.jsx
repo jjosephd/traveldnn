210a5e561ef429188ae739882534730a2f0984c4
jjosephd <jjosephd804@gmail.com>
2023-04-03 23:43:07 -0400

    initial commit including nav

---
+++ src/components/Header/Navigation.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { UilUser } from '@iconscout/react-unicons';
+import { UilFileSearchAlt } from '@iconscout/react-unicons';
+function Navigation() {
+  return (
+    <header className="nav-header">
+      <nav className="navbar">
+        <img src="" alt="" id="logo" />
+        <div className="location-bar">
+          <div id="location">
+            <button>Location</button>
+          </div>
+          <div id="vert-line"></div>
+          <div id="date">
+            <button>Date</button>
+          </div>
+
+          <div id="vert-line"></div>
+          <button id="num-guests">Add Guests</button>
+        </div>
+
+        <div className="options-wrap">
+          <button id="partnerships">Partnerships</button>
+          <button id="reviews-btn">
+            <UilFileSearchAlt id="reviews-icon" />
+          </button>
+          <div className="user-options">
+            <div id="icon-wrap">
+              <UilUser id="user-icon" />
+              <div id="notification"></div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
